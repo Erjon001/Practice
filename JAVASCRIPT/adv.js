e0a3d5ce28b0dd6e2a23a7d6c0ca137ae0c4ab6e
Erjon Rrapi <erjonrrapi429@gmail.com>
2026-01-10 19:22:38 +0100

    JS advanced HW completed

--- JAVASCRIPT/adv.js
+++ JAVASCRIPT/adv.js
@@ -3,13 +3,14 @@ let nextId = 1;
 
 //Exercise 2
 
-const addTask = (title) => {
+const addTask = (title, estimatedMinutes) => {
   const task = {
     id: nextId,
     title: title,
     completed: false,
+    estimatedMinutes,
   };
-  nextId += nextId;
+  nextId++;
   tasks.push(task);
   return task;
 };
@@ -27,7 +28,7 @@ const completeTask = (index) => {
 
 //Exercise 3
 
-const getIncompleteTasks = (tasks) => {
+const getIncompTasks = (tasks) => {
   let incTasks = [];
   for (let i = 0; i < tasks.length; i++) {
     if (tasks[i].completed === false) {
@@ -41,7 +42,7 @@ const getIncompleteTasks = (tasks) => {
 
 //Exercise 4
 
-const incompleteTasks = (tasks) => {
+const getIncompleteTasks = (tasks) => {
   return tasks
     .filter((task) => task.completed === false)
     .map((task) => task.title);
@@ -67,10 +68,10 @@ const completeTaskById = (id) => {
   console.log(`Task completed: ${task.title}, Id: ${id}`);
 };
 
-const deleteTask = (id, index) => {
-  const task = findTaskById(id);
+const deleteTaskById = (id) => {
+  const index = tasks.findIndex((task) => task.id === id);
 
-  if (!task) {
+  if (index === -1) {
     console.log("Invalid id");
     return;
   }
@@ -80,3 +81,80 @@ const deleteTask = (id, index) => {
 };
 
 ////////////////////////////////////////
+
+// Exercise 6
+
+const getCompletedTasks = (tasks) => {
+  return tasks
+    .filter((task) => task.completed === true)
+    .map((task) => task.title);
+};
+
+const countCompletedTasks = (tasks) => {
+  let count = 0;
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].completed === true) {
+      count++;
+    }
+  }
+  return count;
+};
+
+const countIncompleteTasks = (tasks) => {
+  let count = 0;
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].completed === false) {
+      count++;
+    }
+  }
+  return count;
+};
+
+const printSummary = (tasks) => {
+  tasks.forEach((task) => {
+    console.log(
+      `ID: ${task.id}, Task title: ${task.title}, Completed: ${task.completed}`
+    );
+  });
+};
+
+///////////////////////////////////
+
+//Hw P1 already done
+
+//Hw P2
+
+const CloneTask = (tasks) => {
+  return [...tasks];
+};
+
+const getTasksWithAllCompleted = (tasks) => {
+  return tasks.map((task) => ({ ...task, completed: true }));
+};
+
+///////////////////////////////
+
+//HW P3
+
+const getAverageEstimatedMinutes = (tasks) => {
+  let total = 0;
+  let avg = 0;
+  for (let i = 0; i < tasks.length; i++) {
+    total += tasks[i].estimatedMinutes;
+  }
+  avg = total / tasks.length;
+  return avg;
+};
+
+///////////////////////////////////////////
+
+//Optional
+
+const reduceCountIncomplete = (tasks) => {
+  return tasks.reduce((total, task) => {
+    if (task.completed === false) {
+      return total + 1;
+    }
+    return total;
+  }, 0);
+};
